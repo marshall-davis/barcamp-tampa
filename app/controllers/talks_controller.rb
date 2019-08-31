2fdf613ae1966be89687ee8cdeade812afab7dc9
Marshall Davis <toothlessrebel@gmail.com>
2019-08-30 21:57:35 -0400

    Add simplest welcome ever

---
+++ app/controllers/talks_controller.rb
@@ -0,0 +1,3 @@
+class TalksController < ApplicationController
+
+end
---
+++ app/javascript/components/Application.js
@@ -0,0 +1,13 @@
+import React from "react"
+import PropTypes from "prop-types"
+class Application extends React.Component {
+  render () {
+    return (
+      <React.Fragment>
+        Hello? Is this how React works?
+      </React.Fragment>
+    );
+  }
+}
+
+export default Application
