b0aa36ad0653335c6233350a2787132dbb79933d
Marshall Davis <marshall@sourcetoad.com>
2019-09-16 21:42:07 -0400

    Add mutation for Users.

--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -1,7 +1,11 @@
 module Types
   class UserType < Types::BaseObject
     field :id, Integer, null: true
-    field :email, String, null: true
+    field :email, String, null: false
+    field :first_name, String, null: false
+    field :last_name, String, null: true
+    field :twitter, String, null: true
+    field :facebook, String, null: true
     field :talks, [TalkType], null: true
   end
 end
